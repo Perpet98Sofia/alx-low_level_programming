ec73c549ce6c06355afb76d35ebb184f1aefc4b8
Perpet98Sofia <perpetualakwiri@gmail.com>
2023-06-15 15:07:39 -0700

    betty puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
+
 /**
- * main . A C program that prints line with puts function
- * Return:(0)(Success)
+ * main - C program that prints line with puts function
+ * Return:0(Success)
  */
+
 int main(void)
-(
+{
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
-)
+}
