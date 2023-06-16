9d4b2608f0acc2c08a7d6f015759641423e6666c
Perpet98Sofia <perpetualakwiri@gmail.com>
2023-06-16 15:44:41 -0700

    except eq

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: always 0 (success)
+ */
+
+int main(void)
+{
+int lowercase = 'a';
+while (lowercase <= 'z')
+{
+if (lowercase == "e" || lowercase == 'q')
+{
+lowerCase += 1;
+}
+else
+{
+putchar(lowerCase);
+lowercase +=1;
+}
+{
+putchar('\n');
+return(0)
+}
