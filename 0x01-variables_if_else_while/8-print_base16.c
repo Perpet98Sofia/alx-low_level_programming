471f0f26b1460072601495ecfc3ba1a18527747a
Perpet98Sofia <perpetualakwiri@gmail.com>
2023-06-16 17:09:53 -0700

    hexadecimal

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -10,18 +10,13 @@ int main(void)
 char a;
 char b;
 
-a = 48; /* 48 corresponds with 0 on ASCII chart */
-b = 97; /* 97 corresponds with a on ASCII chart */
-
-while (a <= 57) /* 57 corresponds with 9 on ASCII chart*/
+for (a = 48; a <= 57; n++);
 {
-putchar (a); /* this function will output char starting at 48 */
-a++; /* increment putchar operand by 1 until 57 */
+	putchar (n);
 }
-while (b <= 102) /* 102 corresponds with f on ASCII chart */
+for (b = 97; b <= 102; m++);
 {
-putchar (b); /* output char starting at 97  */
-b++; /* increment putchar operand by 1 until 102 */
+	putchar (m);
 }
 putchar ('\n');
 return (0);
