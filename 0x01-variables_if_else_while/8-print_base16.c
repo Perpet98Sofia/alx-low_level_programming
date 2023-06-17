12eeb28d96e50dc8fa6ad43fb513178f8a5acd31
Perpet98Sofia <perpetualakwiri@gmail.com>
2023-06-16 17:13:24 -0700

    hexadecimal

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,5 +1,4 @@
 #include <stdio.h>>
-
 /**
  * main - prints hexadecimal base 0123456789abcdef, using putchar
  * Return: always 0 (success)
@@ -12,11 +11,11 @@ char b;
 
 for (a = 48; a <= 57; n++);
 {
-	putchar (n);
+putchar (n);
 }
 for (b = 97; b <= 102; m++);
 {
-	putchar (m);
+putchar (m);
 }
 putchar ('\n');
 return (0);
