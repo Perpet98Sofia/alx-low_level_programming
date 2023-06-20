2dc61c19d3a865168fd0696db835561d645b9295
Perpet98Sofia <perpetualakwiri@gmail.com>
2023-06-20 07:34:07 -0700

    0-putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,18 @@
 #include "main.h"
-
 /**
- * main - check the code
- *
- * Return: Always 0.
+ * main - prints the string "_putchar".
+ * Return: 0 always (success)
  */
 int main(void)
 {
-    putchar("_putchar\n");
-    return (0);
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');  
+_putchar('r');
+_putchar('\n');
+return (0);
 }
