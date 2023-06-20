a7515cd22102705c72868305f83e7a338ceba8d5
Perpet98Sofia <perpetualakwiri@gmail.com>
2023-06-20 15:05:28 -0700

    alphabet character

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * int _isalpha - checks for alphabetic character
+ * in English alphabet
+ * @c - The character to be tested
+ * Return: 1 if the character is an alphabet
+ * Return: 0 if the character is not an alphabetic character
+ */
+
+int _isalpha(int c)
+{
+if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+return (1);
+else
+return (0);
+}
