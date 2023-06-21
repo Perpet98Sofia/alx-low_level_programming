c8626c20a7fffa7ea2d0c135a88826f15904138e
Perpet98Sofia <perpetualakwiri@gmail.com>
2023-06-21 16:39:09 -0700

    fibonacci.3

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,47 +1,29 @@
-#include 
+#include <stdio.h>
 
 /**
- * main - print first 98 Fibonacci numbers without using long long, malloc,
- * pointers, array/tables, or structures
- * return : 0 always
+ * main - print the first 98 fibonacci
+ * Return: always (0)
  */
 
-int main(void)
+int main() 
 {
-unsigned long int a, a1, a2, b, b1, b2, c, c1, c2, d, e;
+    int i
 
+    int t1 = 0, t2 = 1;
 
-a = 1;
-b = 2;
-c = a + b;
+    int nextTerm = t1 + t2;
 
-printf("%lu, ", a);
-printf("%lu, ", b);
-for (d = 3; d < 89; d++)
-{
-printf("%lu, ", c);
-a = b;
-b = c;
-c = a + b;
-}
-b1 = b / 1000000000;
-b2 = b % 1000000000;
-c1 = c / 1000000000;
-c2 = c % 1000000000;
+    n = 98;
 
+    printf("Fibonacci Series: %d, %d, ", t1,
 
-for (e = 89; e < 98; e++)
-{
-printf("%lu%lu, ", c1, c2);
-a1 = b1;
-a2 = b2;
-b1 = c1;
-b2 = c2;
-c1 = a1 + b1 + ((a2 + b2) / 1000000000);
+    for (i = 3; i <= n; ++i) 
+    {
+        printf("%d, ", nextTerm);
+        t1 = t2;
+        t2 = nextTerm;
+        nextTerm = t1 + t2;
+    }
 
-
-c2 = (a2 + b2) % 1000000000;
-}
-printf("%lu%lu\n", c1, c2);
-return (0);
+    return 0;
 }
