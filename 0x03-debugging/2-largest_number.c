b9e6c4dda06b89fdaa7cb43b4851b4c9edd05af3
Perpet98Sofia <perpetualakwiri@gmail.com>
2023-06-22 14:20:18 -0700

    debugging in C

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -10,30 +10,30 @@
 
 int largest_number(int a, int b, int c)
 {
-    int largest;
-    if (a > b && a > c)
-    {
-        largest = a;
-	{else
-        largest = c;
-	}
-    }
-    {
-    else if (b > a && b > c)
-    }    
-    {
-        largest = b;
-	else
-	largest = c;
-    }
-    else if (c > b)
-    {
-        largest = c;
-    }
-    else
-    {
-	 largest = c;
-    }
+int largest;
 
-    return (largest);
+if (a > b)
+{
+if (a > c)
+largest = a;
+else
+largest = c;
+}
+else if (b > a)
+{
+if (b > c)
+largest = b;
+else
+largest = c;
+}
+else if (a > c)
+{
+largest = a;
+}
+else
+{
+largest = c;
+}
+
+return (largest);
 }
--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -1,24 +1,23 @@
-#include <main.h>
+#include "main.h"
 
 /**
- * main - entry point
+ * positive_or_negative - entry point
+ * Description - checks whether an integer is positive or negative
  * Return: always 0 (success)
  */
-int main(void)
+void positive_or_negative(int i)
 {
-int n;
-
-if (n == 0)
+if (i < 0)
 {
-printf("%d is zero\n", n);
+printf("%d is negative\n", i);
 }
-else if (n > 0)
+ else if (i > 0)
 {
-printf("%d is positive\n", n);
+printf("%d is positive\n", i);
 }
-else
+ else
 {
-printf("%d is negative\n", n);
+printf("%d is zero\n", i);
 }
-return (0);
+return;
 }
