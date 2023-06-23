7dd29f07f8941f810d97631859a75ecfbdee6b16
Perpet98Sofia <perpetualakwiri@gmail.com>
2023-06-23 06:31:35 -0700

    more nested loops

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,14 +1,14 @@
 #include "main.h"
 
 /**
-* _isupper - tests whether a character is a lowercase letter 
+* _isupper - tests whether a character is an uppercase letter 
 *@c: the character to be used
 * Return: 1 if it is a lowercase letter, 0 if it is not a lowercase
 * letter.
 */
 int _isupper(int c)
 {
-if (c >= 'A' && c <= 'Z');
+if (c >= 'A' && c <= 'Z')
 return (1);
 else
 return (0);
