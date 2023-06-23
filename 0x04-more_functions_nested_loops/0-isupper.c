0d916dfd045b44598fc7517c6a9b2eaec3c1e2a7
Perpet98Sofia <perpetualakwiri@gmail.com>
2023-06-22 17:54:05 -0700

    more functions

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+* _isupper - tests whether a character is a lowercase letter 
+*@c: the character to be used
+* Return: 1 if it is a lowercase letter, 0 if it is not a lowercase
+* letter.
+*/
+int _isupper(int c)
+{
+if (c >= 'A' && c <= 'Z')
+return (1);
+else
+return (0);
+}
---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * int mul - multiplies two integers.
+ * @a: integer to be used
+ * @b: integer to be used
+ * Return multiplication of a and b
+ */
+
+int mul(int a, int b)
+{
+return (a * b);
+}
