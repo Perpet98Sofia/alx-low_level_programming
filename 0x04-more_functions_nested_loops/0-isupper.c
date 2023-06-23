5ca6a8a03c2d0f3d15a1ec2a31e4bcf40a7cfac8
Perpet98Sofia <perpetualakwiri@gmail.com>
2023-06-23 10:35:54 -0700

    more nested loops

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
-* _isupper - tests whether a character is an uppercase letter 
+* _isupper - tests whether a character is an uppercase letter
 *@c: the character to be used
 * Return: 1 if it is a lowercase letter, 0 if it is not a lowercase
 * letter.
