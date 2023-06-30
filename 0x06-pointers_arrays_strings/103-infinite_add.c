3a9a213e748e5b64f9ca712dcb64ef102b632005
Perpet98Sofia <perpetualakwiri@gmail.com>
2023-06-30 13:42:21 -0700

    buffer

---
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * infinite_add - adds two strings
+ * @n1: string 1
+ * @n2: string 2
+ * @r: added string
+ * @size_r: buffer of string
+ * Return: r
+ */
+
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
+{
---
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * print_buffer - prints buffer of a char string
+ * @b: buffer to print
+ * @size: size of buffer
+ * Return: nothing
+ */
+
+void print_buffer(char *b, int size)
+{
