3112bfcdb70f564e2b62fa285aa5f15a652385ab
Perpet98Sofia <perpetualakwiri@gmail.com>
2023-07-06 14:16:49 -0700

    wildcmp

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -9,19 +9,22 @@
 
 int wildcmp(char *s1, char *s2)
 {
-	int i = 0, j = 0;
-	while (s1[i] != '\0' && s2[j] != '\0')
+
+	if (*s1 == '\0')
 	{
-		if (s1[i] == s2[j])
-			i++; j++;
-		if (s2[j] == "*")
+		if (*s2 != '\0' && *s2 == '*')
 		{
-			j++;
-			while ((s1[i] != s2[j]) && s1[i] != '\0')
-				i++;
+			return (wildcmp(s1, s2 + 1));
 		}
-		if (s1[i] != s2[j])
-			return (0);
+		return (*s2 == '\0');
 	}
-	return (1);
+	if (*s2 == '*')
+	{
+		return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
+	}
+	else if (*s1 == *s2)
+	{
+		return (wildcmp(s1 + 1, s2 + 1));
+	}
+	return (0);
 }
