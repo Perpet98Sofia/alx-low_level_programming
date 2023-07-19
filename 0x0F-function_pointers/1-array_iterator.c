a5750ca6691cdca967db6e541e1832ca09d7ef9d
Perpet98Sofia <perpetualakwiri@gmail.com>
2023-07-19 14:44:29 -0700

    betty correction

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,7 +1,8 @@
 #include "function_pointers.h"
 
 /**
- *array_iterator - executes a function given as a parameter on each element of an array
+ *array_iterator - executes a function given as a parameter on each
+ *element of an array
  *@array: array to be targeted
  *@size: size of array
  *@action: function to be executed
