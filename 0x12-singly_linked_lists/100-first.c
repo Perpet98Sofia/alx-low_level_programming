89c568828237f4ab5c32475b9cc22cd66c10faec
Perpet98Sofia <perpetualakwiri@gmail.com>
2023-07-28 19:27:19 -0700

    singly linked lists and their elements

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -2,7 +2,7 @@
 
 /**
  * the fnction - prints text before main function executes
- * the function:function that prints statement before the main function is executed.
+ *
  */
 
 void __attribute__ ((constructor)) the function(void)
