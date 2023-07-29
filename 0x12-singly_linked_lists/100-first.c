997b1e3c498b9b0a964e0a39e52b21be95cb413d
Perpet98Sofia <perpetualakwiri@gmail.com>
2023-07-28 19:24:18 -0700

    singly linked lists and their elements

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -2,9 +2,10 @@
 
 /**
  * the fnction - prints text before main function executes
+ * the function:function that prints statement before the main function is executed.
  */
 
-void __attribute__ ((constructor)) esteban(void)
+void __attribute__ ((constructor)) the function(void)
 {
 	printf("You're beat! and yet, you must allow,\n"
 	       "I bore my house upon my back!\n");
